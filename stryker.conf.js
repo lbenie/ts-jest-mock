7a2cfad2fb1503a3e2571f05ba36aa4d7883bf14
Lucien Bénié <lucien.benie@logmein.com>
2021-05-27 09:23:07 -0400

    ci: fix stryker mutation testing

--- stryker.conf.js
+++ stryker.conf.js
@@ -5,8 +5,10 @@ module.exports = {
   packageManager: 'yarn',
   reporters: ['html', 'clear-text', 'progress', 'dashboard'],
   testRunner: 'jest',
-  coverageAnalysis: 'perTest',
+  coverageAnalysis: 'all',
   tsconfigFile: 'tsconfig.spec.json',
-  mutator: 'typescript',
   mutate: ['lib/index.ts'],
+  jest: {
+    configFile: 'jest.config.js',
+  },
 }
