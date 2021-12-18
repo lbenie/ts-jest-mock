511e272827ac36d6e1371c5e009fc78ac83b21e5
Lucien Bénié <lbenie@users.noreply.github.com>
2021-12-18 16:26:47 +0000

    chore: update release config

--- release.config.js
+++ release.config.js
@@ -63,7 +63,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
   ],
